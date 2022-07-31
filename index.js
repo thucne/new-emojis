3e96c624fe13bb89f21df39e9214c3d02bb37261
Trong Thuc - 162864 <trongthuc.bentre@gmail.com>
2022-07-31 23:17:20 +0700

    ud

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 import path from "path";
 import favicon from "serve-favicon";
 
@@ -14,20 +13,7 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-if (process.env.NODE_ENV === "development") {
-    app.use(cors({ origin: `http://localhost:3000` }));
-} else {
-    app.use(
-        cors({
-            origin: [
-                /trantrongthuc\.com$/,
-                /thuc\.tech$/,
-            ],
-        })
-    );
-}
-
-
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json({ limit: "150mb" }));
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
@@ -40,14 +26,6 @@ app.use((req, res, next) => {
 
 app.use("/", routes);
 
-mongoose
-    .connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log(`Database connected`);
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.log(`Error in connecting MongoDB: ${err.message}`);
-    });
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
